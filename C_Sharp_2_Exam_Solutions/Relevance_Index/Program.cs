377c9d890e11db4c4250850689713cc6e0a3f3c0
Gyokay Ahmed <gyokayakmed@gmail.com>
2016-05-02 12:12:54 +0300

    Added solutin for Relevance Index problem

---
+++ C_Sharp_2_Exam_Solutions/Relevance_Index/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Relevance_Index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int l = int.Parse(Console.ReadLine());
+            char[] splitCharacters = new char[] { ' ', ',', '.', '?', '!', ';', '-', ')', '(' };
+
+            Dictionary<string[], int> paragraphs = new Dictionary<string[], int>();
+            
+            for (int i = 0; i < l; i++)
+            {
+                string inputString = Console.ReadLine();
+
+                string[] currentParagraph = inputString.Split(splitCharacters, StringSplitOptions.RemoveEmptyEntries);
+
+                ToUpperCase(currentParagraph, word);
+
+                paragraphs.Add(currentParagraph, CountStringOccurrences(currentParagraph, word));
+            }
+
+            var items = from pair in paragraphs
+                        orderby pair.Value descending
+                        select pair;
+
+            foreach (var item in items)
+            {
+                PrintStringArr(item.Key);
+            }
+        }
+
+        public static int CountStringOccurrences(string[] stringArr, string word)
+        {
+            int count = 0;
+
+            for (int i = 0; i < stringArr.Length; i++)
+            {
+                if (stringArr[i] == word.ToUpper())
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        static void ToUpperCase(string[] initialStringArr, string word)
+        {
+            for (int i = 0; i < initialStringArr.Length; i++)
+            {
+                if (string.Equals(initialStringArr[i], word, StringComparison.CurrentCultureIgnoreCase)) // case insensitive string comparison
+                {
+                    initialStringArr[i] = initialStringArr[i].ToUpper();
+                }
+            }
+        }
+
+        static void PrintStringArr(string[] strArr)
+        {
+            StringBuilder stringToPrint = new StringBuilder();
+
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                stringToPrint.Append(strArr[i]);
+
+                if (i < strArr.Length - 1)
+                {
+                    stringToPrint.Append(" ");
+                }
+            }
+
+            Console.WriteLine(stringToPrint);
+        }
+    }
+}
